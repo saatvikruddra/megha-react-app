bdea37302b3420cdbdba725d4e080af99d441bbe
cto_varun <cto.varun@gmail.com>
2018-08-27 12:40:37 +0530

    state done

--- src/Contact.js
+++ src/Contact.js
@@ -3,13 +3,35 @@ import React, { Component } from 'react';
 class Contact extends Component{
     constructor(props){
         super(props);
-        console.log(this.props);
+        console.log("Constructor of contact component ");
+        this.state = {
+            myNumber: this.props.myNumber,
+            myEmail: this.props.myEmail
+        };
+    }
+    componentWillMount() {
+        console.log("Component willmount of contact component ");
+    }
+    componentDidMount() {
+        console.log("Component did mount of contact component fired");
+    }
+    changeValue = () => {
+        // this.props.myNumber = "8699643192";
+        this.setState({
+            myNumber: "8699643192",
+            myEmail: 'varunrocks27.sharma@gmail.com'
+        });
     }
     render(){
+        console.log("render function fired");
         return(
-            <p>Contact NUGEN on {this.props.myNumber} and email is {this.props.myEmail}</p>
+            <div>
+            <p>Contact NUGEN on {this.state.myNumber} and email is {this.state.myEmail}</p>
+            <button onClick={this.changeValue}>Change Value</button>
+            </div>
         );
     }
+    
 }
 
 export default Contact;
